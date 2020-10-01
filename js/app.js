087357a45fa31e008b1cf99d01814b348bf1d9c7
nathokwoli <nathworld2@gmail.com>
2020-10-01 19:43:22 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function($, document, window){
+
+	$('.plans').hover(function(){
+
+		$('.plan-nav').fadeToggle().delay(2000);
+	});
+
+
+	$(document).ready(function(){
+		$(".slider").flexslider({
+			animation: "fade",
+			controlNav: true,
+			directionNav: false,
+		});
+
+		$(".menu-toggle").click(function(){
+			$(".main-navigation .menu").slideToggle();
+		});
+
+		$("[data-bg-color]").each(function(){
+			var color = $(this).data("bg-color");
+			$(this).css("background-color",color);
+		});
+
+		new WOW().init();
+	});
+
+	$(window).ready(function(){
+
+	});
+
+})(jQuery, document, window);
